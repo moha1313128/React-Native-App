c0a50d42c0a0be988da90ca5e450c2b07a536ad7
moha1313128 <moha1313128@gmail.com>
2020-01-18 22:04:17 +0100

    Scroll View

--- App.js
+++ App.js
@@ -1,52 +1,46 @@
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] =  useState('Mohammed');
-  const [age, setAge] = useState('40');
-  // const clickHandler = () => {
-    // setName('chun-li');
-    // setPerson({name: 'Cobras', age: 90});
-  // }
+  const [people, setPeople] = useState([
+    { name: 'moha1', key: '1' },
+    { name: 'moha2', key: '2' },
+    { name: 'moha3', key: '3' },
+    { name: 'moha4', key: '4' },
+    { name: 'moha5', key: '5' },
+    { name: 'moha6', key: '6' },
+    { name: 'moha7', key: '7' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Entre your name:</Text>
-      <TextInput style={styles.input}
-        multiline
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)} />
-
-      <Text>Entre your age:</Text>
-      <TextInput style={styles.input}
-        placeholder='e.g. 90'
-        keyboardType='numeric'
-        onChangeText={(val) => setAge(val)} />
-
-      <Text>My name is {name} and my age is {age}</Text>
-      {/* <Text>His name is {person.name} and his age is {person.age}</Text> */}
-      {/* <View style={styles.btnConatiner}> */}
-        {/* <Button title='update state' onPress={clickHandler} /> */}
-      {/* </View> */}
+      <ScrollView>
+        { people.map(item => (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  btnContauner: {
-    marginTop: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 18,
-    width: 200,
-  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 40
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'whitesmoke',
+    fontSize: 24,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    borderRadius: 50
+  }
 });
