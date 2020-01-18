c50cf7a7ce988dfd284c866af6e7e1d653cf1224
moha1313128 <moha1313128@gmail.com>
2020-01-18 21:46:43 +0100

    Using State & setState On Text_Input

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] =  useState('Mohammed');
+  const [age, setAge] = useState('40');
+  // const clickHandler = () => {
+    // setName('chun-li');
+    // setPerson({name: 'Cobras', age: 90});
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Entre your name:</Text>
+      <TextInput style={styles.input}
+        multiline
+        placeholder='e.g. John Doe'
+        onChangeText={(val) => setName(val)} />
+
+      <Text>Entre your age:</Text>
+      <TextInput style={styles.input}
+        placeholder='e.g. 90'
+        keyboardType='numeric'
+        onChangeText={(val) => setAge(val)} />
+
+      <Text>My name is {name} and my age is {age}</Text>
+      {/* <Text>His name is {person.name} and his age is {person.age}</Text> */}
+      {/* <View style={styles.btnConatiner}> */}
+        {/* <Button title='update state' onPress={clickHandler} /> */}
+      {/* </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  btnContauner: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 18,
+    width: 200,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
