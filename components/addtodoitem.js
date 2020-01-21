fb2afae1ddf893f7272585c0428347b83c89cb86
moha1313128 <moha1313128@gmail.com>
2020-01-21 22:05:20 +0100

    Styling Buuton

--- components/addtodoitem.js
+++ components/addtodoitem.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
-
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function AddTodoItem({ submitHandler }) {
 
@@ -15,7 +16,14 @@ export default function AddTodoItem({ submitHandler }) {
             placeholder='Add todo'
             onChangeText={changeHandler}
             />
-            <Button onPress={() => submitHandler(text)} title='Submit' color='coral' />
+            {/* <Ionicons name='ios-add-circle' size={30} style={{height:25, width:25}} color= >
+                <Text style={styles.btn} onPress={() => submitHandler(text)}>SUBMIT</Text>
+            </Ionicons>     */}
+            <FontAwesome.Button name="plus-circle" backgroundColor="#3b5998">
+                {/* <Text style={styles.btn} onPress={() => submitHandler(text)}>SUBMIT</Text> */}
+                <Button onPress={() => submitHandler(text)} title='Submit' color='#3b5998' />
+            </FontAwesome.Button>
+            {/* <Button onPress={() => submitHandler(text)} title='Submit' color='coral' /> */}
         </View>
     )
 }
@@ -26,11 +34,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 8,
         paddingVertical: 6,
         borderBottomWidth: 1,
-        borderBottomColor: '#ddd'
+        borderBottomColor: '#3b5998'
     },
     btn: {
-        width: 100,
-        color: 'blue'
+        color:'#fff',
+        // justifyContent: 'center',
+        // textAlign: 'center',
     }
 })
 
